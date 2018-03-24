56cafbdff8d005d4336686b37ab3e2fd551bbb03
evelyn <edtwinfish@gmail.com>
2018-03-23 22:29:13 -0700

    initial commit for canine age calculator

--- src/com/edtwinfish/Main.java
+++ src/com/edtwinfish/Main.java
@@ -3,6 +3,8 @@ package com.edtwinfish;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// prompt user for canine's age in human years
+    // pass user entry to method to perform calculation
+    // return canine years to user
     }
 }
